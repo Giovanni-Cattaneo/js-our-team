0237f646b4be5874be1e7ac4b331c419356403bc
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-04 15:50:10 +0100

    add card with img

--- main.js
+++ main.js
@@ -2,53 +2,56 @@ console.log("hello world");
 
 const team = [
     {
-    name: 'Wayne Barnett',
-    role: 'Founder & CEO',
-    image: './img/wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: './img/wayne-barnett-founder-ceo.jpg',
     },
     {
-    name: 'Angela Caroll',
-    role: 'Chief Editor',
-    image: './img/angela-caroll-chief-editor.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: './img/angela-caroll-chief-editor.jpg',
     },
     {
-    name: 'Walter Gordon',
-    role: 'Office Manager',
-    image: './img/walter-gordon-office-manager.jpg',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: './img/walter-gordon-office-manager.jpg',
     },
     {
-    name: 'Angela Lopez',
-    role: 'Social Media Manager',
-    image: './img/angela-lopez-social-media-manager.jpg',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: './img/angela-lopez-social-media-manager.jpg',
     },
     {
-    name: 'Scott Estrada',
-    role: 'Developer',
-    image: './img/scott-estrada-developer.jpg',
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: './img/scott-estrada-developer.jpg',
     },
     {
-    name: 'Barbara Ramos',
-    role: 'Graphic Designer',
-    image: './img/barbara-ramos-graphic-designer.jpg',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: './img/barbara-ramos-graphic-designer.jpg',
     },
-    ];
-    
-    
+];
 
-    for (let i = 0; i < team.length; i++) { // ciccliamo per estrapolare i vari membri del team
-        const member = team[i];
-        
-        const container = document.querySelector(".container")
 
-        const markupName = `<div class="member">Nome:${member.name}</div>`
 
-        const markupRole = `<div class="member">${member.role}</div>`
+for (let i = 0; i < team.length; i++) { // ciccliamo per estrapolare i vari membri del team
+    const member = team[i];
 
-        const markupImage = `<img src="${member.image}" alt="">`
+    const container = document.querySelector(".container")
 
-        container.innerHTML += (markupName + markupRole + markupImage)
-    }
+    const markup = `
+    <div class="card m-2 pb-5">
+    <img class=" m-auto " src="${member.image}" alt="">
+    <div class="card-body">
+      <h5 class="card-title">${member.name}</h5>
+      <p class="card-text">${member.role}</p>
+    </div>
+    </div>`
 
-    
 
-    
+    container.innerHTML += (markup)
+}
+
+
+
